63c4733ddf71ade2ccb0c6bd6063ce6b739fafc7
Jesse Gay <jesse@teamtreehouse.com>
2018-08-21 19:20:05 -0700

    Created QuestionsStruct in TriviaCollection
    Had to add lazy keyword. Why?

--- EnhanceQuizStarter/QuizManager.swift
+++ EnhanceQuizStarter/QuizManager.swift
@@ -12,3 +12,5 @@ import Foundation
  QuizManager ​object ​which ​could ​initialize ​the ​Questions ​and ​add ​them ​to ​the
  Quiz, ​randomize ​the ​set ​of ​Questions, ​check ​if ​the ​answer ​is ​correct, ​etc.
  */
+
+//I think I can move most a BUNCH of the quiz parameters from ViewDidLoad into here (questions asked, questions per round, correct answers, the mechanism for picking a random number that is used as an index to get a question from the TriviaCollection object
